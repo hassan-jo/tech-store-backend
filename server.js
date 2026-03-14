6117656aa72c45ebc569b49352ef6d54fd9c5ce2
hassan-jo <hassanmoahmed09@gmail.com>
2026-03-14 13:13:10 -0700

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: "https://hassantechh.netlify.app",
+    origin: ["https://hassantechh.netlify.app", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
