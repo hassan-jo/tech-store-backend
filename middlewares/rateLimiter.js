7edea419b6be8ac82e888504fffaed1f0227d9b2
hassan-jo <hassanmoahmed09@gmail.com>
2026-03-15 08:41:51 -0700

    Update rateLimiter.js

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,17 +2,17 @@ const rateLimit = require("express-rate-limit");
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 5,
+  max: 50,
 });
 
 const registerLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 10,
+  max: 50,
 });
 
 const refreshLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 40,
+  max: 100,
 });
 
 module.exports = {
